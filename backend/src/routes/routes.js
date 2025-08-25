c4b4f84c72071da2ab80867cafbd67ae0c807c93
hector_ <danielvargas.nike@gmail.com>
2025-08-24 23:56:16 -0500

    adding routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -9,19 +9,34 @@ import {
     showDeleteUserEmail
 } from "../controllers/user.controller.js";
 
+// endpoint route
 const router = Router()
 
+
+// brings all users
 router.get('/users', showGetAllUser)
+
+
 
 router.get('/users/:id_user', showGetUserId)
 
+
+
 router.get('/users/email/:email', showGetUserEmail)
+
+
 
 router.post('/users/create', showCreateUser)
 
-router.put('/users/update', showUpdateUser)
+
+
+router.put('/users/update:id_user', showUpdateUser)
+
+
 
 router.delete('/users/delete/:id_user', showDeleteUserId)
+
+
 
 router.delete('/users/delete/email/:email', showDeleteUserEmail)
 
