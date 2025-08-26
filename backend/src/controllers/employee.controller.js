5c80a96c2e615bb32c255122a2922b355d9968ec
hector_ <danielvargas.nike@gmail.com>
2025-08-26 02:07:13 -0500

    adding employee controller

---
+++ backend/src/controllers/employee.controller.js
@@ -0,0 +1,78 @@
+import { 
+    allEmployee,
+    searchEmployeeById,
+    newEmployee,
+    deleteEmployee
+} from "../services/employees.js";
+
+
+export const viewsAllEmployee = async (req, res) => {
+    try {
+        const employee = await allEmployee()
+        res.status(200).json(employee)
+    } catch (error) {
+        console.log('Error:', error.message);
+        
+        res.status(404).json({
+            message: 'Error al obtener empleados',
+            error: error.message
+        })
+    }
+};
+
+
+export const viewsEmployeeById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const employeeId = await searchEmployeeById(id)
+        res.status(200).json(employeeId)
+    } catch (error) {
+        console.log('Error: ', error.message);
+        
+        res.status(404).json({
+            message: 'Error al obtener empleado por id',
+            error: error.message
+        })
+        
+    }
+};
+
+
+export const viewsNewEmployee = async (req, res) => {
+    try {
+        const { 
+            full_name,
+            identification_number,
+            phone,
+            charge_id,
+            company_id,
+            user_id
+        } = req.body
+        const employeeNew = await newEmployee(full_name, identification_number, phone, charge_id, company_id, user_id);
+        res.status(201).json(employeeNew);
+    } catch (error) {
+        console.log('ERROR: ', error.message);
+        res.status(404).json({
+            message: 'Error al crear nuevo empleado',
+            error: error.message
+        })
+        
+    }
+};
+
+
+export const viewsDeleteEmployees = async (req, res) => {
+    try {
+        const { id } = req.params
+        const doAwayEmployee = await deleteEmployee(id) 
+        res.status(200).json(doAwayEmployee);
+    } catch (error) {
+        console.log('Error: ', error.message);
+        res.status(404).json({
+            message: 'Error al eliminar empleado',
+            error: error.message
+        })
+        
+        
+    }
+}
