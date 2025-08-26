318d9b687754324bfb6121ab76ad1a95d60292c4
hector_ <danielvargas.nike@gmail.com>
2025-08-26 02:08:54 -0500

    adding employee models

---
+++ backend/src/models/employee_queries.js
@@ -0,0 +1,51 @@
+import { pool } from "../../db/db.js";
+
+
+// GET
+export const getEmployee = async () => {
+    const result = await pool.query(`SELECT * FROM employees;`)
+    return result.rows
+};
+
+
+export const getEmployeeById = async (employee_id) => {
+    const query = `SELECT * FROM employees WHERE employee_id = $1;`;
+    const values = [employee_id]
+    const result = await pool.query(query, values)
+    return result.rows[0]
+};
+
+
+// POST
+export const postEmployess = async (full_name, identification_number, phone, charge_id, company_id, user_id) => {
+    const query = `INSERT INTO employees (full_name, identification_number, phone, charge_id, company_id, user_id
+                VALUES ($1, $2, $3, $4, $5, $6);`;
+    const values = [full_name, identification_number, phone, charge_id, company_id, user_id]
+    const result = await pool.query(query, values)
+    return result.rows[0]
+};
+
+
+// PUT
+export const putEmployeesById = async (full_name, identification_number, phone, charge_id, company_id) => {
+    const query = `UPDATE employees 
+                SET full_name = $1, identification_number = $2, phone = $3, charge_id = $4, company_id = $5;`;
+    const values = [full_name, identification_number, phone, charge_id, company_id, user_id]
+    const result = await pool.query(query, values)
+    return result.rows[0]
+};
+
+
+
+// DELETE 
+export const deleteEmployeesById = async (employee_id) => {
+    const query = `SELECT FROM employees WHERE employee_id = $1;`
+    const values = [employee_id]
+    const result = await pool.query(query, values)
+    return result.rows[0]
+};
+
+
+
+
+
